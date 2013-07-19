9dc585c8911f698dea985e252abfc600f174fd9c
Ubuntu <ubuntu@sunilksh.(none)>
2013-07-19 21:19:24 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 app.use(express.logger());
-
+var buffer = buffer(100);
+buffer = fs.readFile('./index.html');
+var str = buffer.toString('utf8,0,buffer.length);
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send(str);
 });
 
 var port = process.env.PORT || 5000;
