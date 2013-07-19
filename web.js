cd187c7043c26431cd953c4612b20fd9fea4aaef
Ubuntu <ubuntu@sunilksh.(none)>
2013-07-19 21:45:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var app = express();
 var fs = require('fs');
 
 app.use(express.logger());
-var buf = buffer(100);
-buf = fs.readFileSync('/home/ubuntu/bitstarter/index.html');
-var str = buf.toString('utf8',0,buf.length);
+
+var buf = fs.readFileSync('/home/ubuntu/bitstarter/index.html','utf8');
+
 app.get('/', function(request, response) {
-  response.send(str);
+  response.send(buf);
 });
 
 var port = process.env.PORT || 5000;
